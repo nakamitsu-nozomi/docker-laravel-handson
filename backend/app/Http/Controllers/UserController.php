da17c5deb32c65e791b8da5d0427d2770c207e2f
nozomi <kkk@docomo.ne.jp>
2021-03-10 11:12:20 +0900

    マイページに認可設定

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -4,13 +4,19 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class UserController extends Controller
 {
     public function show(Request $request, string $name)
     {
+
         $user = User::where("name", $name)->first();
-        $locations = $user->locations->sortByDesc("created_at");
-        return view("users.show", compact("locations", "user"));
+        if (Auth::id() === $user->id) {
+            $locations = $user->locations->sortByDesc("created_at");
+            return view("users.show", compact("locations", "user"));
+        } else {
+            return redirect()->route("users.show", ["name" => $request->user()->name]);
+        }
     }
 }
