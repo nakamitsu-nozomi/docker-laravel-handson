429432f7cc76ab75df8bb293bc551488cb60df50
nozomi <kkk@docomo.ne.jp>
2021-03-11 20:40:36 +0900

    5日分の天気予報を表示

--- backend/app/Http/Controllers/UserController.php
+++ backend/app/Http/Controllers/UserController.php
@@ -20,33 +20,101 @@ class UserController extends Controller
 
         // ユーザーの表示
         $user = User::where("name", $name)->first();
+        $locations = $user->locations->sortByDesc("created_at");
         if (Auth::id() === $user->id) {
-            $locations = $user->locations->sortByDesc("created_at");
             $weathers = [];
             $temp_maxs = [];
             $temp_mins = [];
             $weather_icons = [];
-            foreach ($locations as $location) {
+            $dates = [];
+            $times = [];
+            foreach ($locations  as $location) {
                 $zipcode = User::zipcode($location->zipcode);
-                $response = User::getWeather('weather', $zipcode);
-                array_push($weathers, User::getTranslation($response['weather'][0]['description'])); // 天気
-                array_push($temp_maxs, $response['main']['temp_max']); // 最高気温
-                array_push($temp_mins, $response['main']['temp_min']); // 最低気温
-                array_push($weather_icons, $response['weather'][0]['icon']); // 天気マーク
-            }
-            for ($s = 0; $s < count($locations); $s++) {
-                $output[] = array($weathers[$s], $temp_maxs[$s], $temp_mins[$s], $weather_icons[$s]);
-                $outputs[] = array($weathers[$s], $temp_maxs[$s], $temp_mins[$s], $weather_icons[$s], $locations[$s]);
+                $response = User::getWeather('forecast', $zipcode);
+                $weather_list = $response["list"];
+                foreach ($weather_list as $items) {
+                    array_push($weathers, User::getTranslation($items['weather'][0]['description'])); // 天気
+                    array_push($temp_maxs, $items['main']['temp_max']); // 最高気温
+                    array_push($temp_mins, $items['main']['temp_min']); // 最低気温
+                    array_push($weather_icons, $items['weather'][0]['icon']); // 天気マーク
+                    $datetime = new DateTime();
+                    $datetime->setTimestamp($items['dt'])->setTimeZone(new DateTimeZone('Asia/Tokyo')); // 日時 - 協定世界時 (UTC)を日本標準時 (JST)に変換
+                    array_push($dates, $datetime->format('Y年m月d日')); // 日付
+                    array_push($times, $datetime->format('H:i')); // 時間
+
+                }
             }
 
+            // for ($s = 0; $s < count($locations); $s++) {
+            //     $output[] = array($weathers[$s], $temp_maxs[$s], $temp_mins[$s], $weather_icons[$s]);
+            //     $outputs[] = array($weathers[$s], $temp_maxs[$s], $temp_mins[$s], $weather_icons[$s], $locations_[$s]);
+            // }
 
-            $datetime = new DateTime();
-            $datetime->setTimestamp($response['dt'])->setTimeZone(new DateTimeZone('Asia/Tokyo')); // 日時 - 協定世界時 (UTC)を日本標準時 (JST)に変換
-            $date =  $datetime->format('Y年m月d日'); //日付
-            $time = $datetime->format('H:i'); // 時間
-            return view("users.show", compact("locations", "user", "outputs", "date"));
+
+
+
+            // $datetime = new DateTime();
+            // $datetime->setTimestamp($response['dt'])->setTimeZone(new DateTimeZone('Asia/Tokyo')); // 日時 - 協定世界時 (UTC)を日本標準時 (JST)に変換
+            // $date =  $datetime->format('Y年m月d日'); //日付
+            // $time = $datetime->format('H:i'); // 時間
+
+
+
+
+            return view("users.show", compact("temp_maxs", "user", "weathers", "temp_mins", "weather_icons", "locations", "dates", "times"));
         } else {
             return redirect()->route("users.show", ["name" => $request->user()->name]);
         }
     }
 }
+
+
+// $locations = $user->locations->sortByDesc("created_at");
+// $weathers = [];
+// $temp_maxs = [];
+// $temp_mins = [];
+// $weather_icons = [];
+// foreach ($locations as $location) {
+//     $zipcode = User::zipcode($location->zipcode);
+//     $response = User::getWeather('weather', $zipcode);
+//     array_push($weathers, User::getTranslation($response['weather'][0]['description'])); // 天気
+//     array_push($temp_maxs, $response['main']['temp_max']); // 最高気温
+//     array_push($temp_mins, $response['main']['temp_min']); // 最低気温
+//     array_push($weather_icons, $response['weather'][0]['icon']); // 天気マーク
+// }
+// for ($s = 0; $s < count($locations); $s++) {
+//     $output[] = array($weathers[$s], $temp_maxs[$s], $temp_mins[$s], $weather_icons[$s]);
+//     $outputs[] = array($weathers[$s], $temp_maxs[$s], $temp_mins[$s], $weather_icons[$s], $locations[$s]);
+// }
+
+
+// $datetime = new DateTime();
+// $datetime->setTimestamp($response['dt'])->setTimeZone(new DateTimeZone('Asia/Tokyo')); // 日時 - 協定世界時 (UTC)を日本標準時 (JST)に変換
+// $date =  $datetime->format('Y年m月d日'); //日付
+// $time = $datetime->format('H:i'); // 時間
+// return view("users.show", compact("locations", "user", "outputs", "date"));
+
+
+
+
+// $data = [];
+// foreach ($locations as $location) {
+//     $zipcode = User::zipcode($location->zipcode);
+//     $response = User::getWeather('forecast', $zipcode);
+//     array_push($data, $response["list"]);
+//     // foreach ($weather_list as $items) :
+//     //     $temp = $items['main']['temp']; // 気温
+//     //     $temp_max = $items['main']['temp_max']; // 最高気温
+//     //     $temp_min = $items['main']['temp_min']; // 最低気温
+//     //     $humidity = $items['main']['humidity']; // 湿度
+//     //     $weather = $items['weather'][0]['main']; // 天気
+
+//     //     // $weather_icon = $items['weather'][0]['icon']; // 天気アイコン（公式のアイコンを使用）
+//     //     $datetime = new DateTime();
+//     //     $datetime->setTimestamp($items['dt'])->setTimeZone(new DateTimeZone('Asia/Tokyo')); // 日時 - 協定世界時 (UTC)を日本標準時 (JST)に変換
+//     //     $date =  $datetime->format('Y年m月d日'); //　日付
+//     //     $time = $datetime->format('H:i'); // 時間
+
+//     // endforeach;
+
+// }
--- backend/resources/views/users/show.blade.php
+++ backend/resources/views/users/show.blade.php
@@ -6,79 +6,96 @@
 @section('content')
 
   <div class="container">
-      {{-- @foreach (array_map(NULL, $locations, $weathers,$temp_maxs,$temp_mins,$weather_icons) as [ $location,$weather, $temp_max,$temp_min,$weather_icon ]) --}}
-      @foreach($outputs as $output)
-      <div class="card mt-3">
-        <div class="card-body d-flex flex-row">
-          <i class="fas fa-user-circle fa-3x mr-1"></i>
-          <div>
-            <div class="font-weight-bold">
-            ユーザー名:  {{$user->name}}
-            </div> 
-            <div class="font-weight-lighter">
-            </div>
-          </div>
-          @if( Auth::id() === $output[4]->user_id )
-          <!-- dropdown -->
-          <div class="ml-auto card-text">
-            <div class="dropdown">
-              <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                <button type="button" class="btn btn-link text-muted m-0 p-2">
-                  <i class="fas fa-ellipsis-v"></i>
-                </button>
-              </a>
-              <div class="dropdown-menu dropdown-menu-right">
-                <a class="dropdown-item" href="{{route("locations.edit",["location" =>$output[4]])}}">
-                  <i class="fas fa-pen mr-1"></i>位置情報を更新する
-                </a>
-                <div class="dropdown-divider"></div>
-                <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $output[4]->id }}">
-                  <i class="fas fa-trash-alt mr-1"></i>位置情報を削除する
-                </a>
-              </div>
-            </div>
-          </div>
-          <!-- dropdown -->
-  
-          <!-- modal -->
-          <div id="modal-delete-{{ $output[4] ->id }}" class="modal fade" tabindex="-1" role="dialog">
-            <div class="modal-dialog" role="document">
-              <div class="modal-content">
-                <div class="modal-header">
-                  <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
-                    <span aria-hidden="true">&times;</span>
-                  </button>
-                </div>
-                <form method="POST" action="{{ route('locations.destroy', ['location' => $output[4]]) }}">
-                  @csrf
-                  @method('DELETE')
-                  <div class="modal-body">
-                    郵便番号【{{ $output[4]->zipcode }}】を削除します。よろしいですか？
-                  </div>
-                  <div class="modal-footer justify-content-between">
-                    <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
-                    <button type="submit" class="btn btn-danger">削除する</button>
-                  </div>
-                </form>
-              </div>
-            </div>
-          </div>
-          <!-- modal -->
-        @endif
-        </div>
-        <div class="card-body pt-0 pb-2">
-          <h3 class="h4 card-title">
-            郵便番号：  {{$output[4]->zipcode}}
-          </h3>
-          <h3 class="h4 card-title"> {{$date}}の天気</h3>
-          <p> {{$output[0]}}</p>
-           <img src="https://openweathermap.org/img/wn/<?php echo $output[3]; ?>@2x.png">
-            <p>最高気温:{{$output[1]}}℃ <span>最低気温：{{$output[2]}}℃</span></p>
+
+         @foreach ($locations as $location)
+
+
+
+  <div class="card mt-3">
+    <div class="card-body d-flex flex-row">
+      <i class="fas fa-user-circle fa-3x mr-1"></i>
+      <div>
+        <div class="font-weight-bold">
+        ユーザー名:  {{$user->name}}
+        </div> 
+        <div class="font-weight-lighter">
         </div>
       </div>
-      @endforeach
+      @if( Auth::id() === $location->user_id )
+      <!-- dropdown -->
+      <div class="ml-auto card-text">
+        <div class="dropdown">
+          <a data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+            <button type="button" class="btn btn-link text-muted m-0 p-2">
+              <i class="fas fa-ellipsis-v"></i>
+            </button>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right">
+            <a class="dropdown-item" href="{{route("locations.edit",["location" =>$location])}}">
+              <i class="fas fa-pen mr-1"></i>位置情報を更新する
+            </a>
+            <div class="dropdown-divider"></div>
+            <a class="dropdown-item text-danger" data-toggle="modal" data-target="#modal-delete-{{ $location->id }}">
+              <i class="fas fa-trash-alt mr-1"></i>位置情報を削除する
+            </a>
+          </div>
+        </div>
+      </div>
+      <!-- dropdown -->
+
+      <!-- modal -->
+      <div id="modal-delete-{{ $location ->id }}" class="modal fade" tabindex="-1" role="dialog">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <button type="button" class="close" data-dismiss="modal" aria-label="閉じる">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <form method="POST" action="{{ route('locations.destroy', ['location' => $location]) }}">
+              @csrf
+              @method('DELETE')
+              <div class="modal-body">
+                郵便番号【{{ $location->zipcode }}】を削除します。よろしいですか？
+              </div>
+              <div class="modal-footer justify-content-between">
+                <a class="btn btn-outline-grey" data-dismiss="modal">キャンセル</a>
+                <button type="submit" class="btn btn-danger">削除する</button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+      <!-- modal -->
+    @endif
+    </div>
+    <div class="card-body pt-0 pb-2">
+      <h3 class="h4 card-title">
+        郵便番号：  {{$location->zipcode}}
+      </h3>
+      @foreach (array_map(null, $weathers, $temp_maxs,$weather_icons,$temp_mins,$dates,$times) as [$weather,$temp_max,$weather_icon,$temp_min,$date,$time])
+      <h3 class="h4 card-title"> {{$date}}{{$time}}</h3>
+      <p> {{$weather}}</p>
+        <img src="https://openweathermap.org/img/wn/<?php echo $weather_icon; ?>@2x.png">
+        <p>最高気温:{{$temp_max}}℃ <span>最低気温：{{$temp_min}}℃</span></p>
+        @endforeach
+    </div>
+  </div>
+  
+
+  @endforeach
   </div>
 @endsection
 
 
 
+
+
+
+
+
+
+  {{-- @foreach (array_map(null, $weathers, $temp_maxs,$weather_icons) as [$val1, $val2,$weather_icon])
+    {{$val1}},{{$val2}}
+
+@endforeach --}}
