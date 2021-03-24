935c67352e62ce03da8c0b95cce4440df121cb11
nozomi <kkk@docomo.ne.jp>
2021-03-24 14:28:02 +0900

    LocationのCRUDテスト実装

--- backend/tests/Feature/LocationControllerTest.php
+++ backend/tests/Feature/LocationControllerTest.php
@@ -8,6 +8,7 @@ use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+
 use Tests\TestCase;
 use App\User;
 
@@ -57,6 +58,31 @@ class LocationControllerTest extends TestCase
 
 
     ### 投稿機能のテスト ###
+    // 未ログイン時
+    public function testGeststore()
+    {
+        $response = $this->post(route("locations.store"));
+        $response->assertRedirect("login");
+    }
+    // 未ログイン時
+
+    public function testAuthstore()
+    {
+        $user = factory(User::class)->create();
+        $location = factory(Location::class)->create();
+        $response = $this->actingAs($user)->post(route("locations.store", [
+            "zipcode" => $location->zipcode,
+            "address" => $location->address,
+            "user_id" => $location->user_id,
+        ]));
+
+        $this->assertDatabaseHas('locations', [
+            "zipcode" => $location->zipcode,
+            "address" => $location->address,
+            "user_id" => $location->user_id,
+        ]);
+        $response->assertRedirect("/");
+    }
 
     ### 投稿の編集画面 表示機能のテスト ###
 
@@ -67,7 +93,7 @@ class LocationControllerTest extends TestCase
         $response = $this->get(route('locations.edit', ["location" => $location]));
         $response->assertRedirect(route('login'));
     }
-    // ログイン時
+    // 未ログイン時
     public function testAuthUseredit()
     {
         $location = factory(Location::class)->create();
@@ -76,6 +102,27 @@ class LocationControllerTest extends TestCase
         $response->assertStatus(200)->assertViewIs('locations.edit', ["location" => $location]);
     }
     ### 投稿削除機能のテスト ###
-
-
+    public function testDestroy()
+    {
+        $this->withExceptionHandling();
+        // テストデータをDBに保存
+        $user = factory(User::class)->create();
+        $zipcode = 5610861;
+        $address = "大阪府豊中市東泉丘";
+        $user_id = $user->id;
+        $location = Location::create([
+            "zipcode" =>  $zipcode,
+            "address" => $address,
+            "user_id" => $user->id,
+        ]);
+        // DBかデータを削除
+        $response = $this->actingAs($user)->delete(route("locations.destroy", ["location" => $location]));
+        // テストデータが消えているか確認
+        $this->assertDeleted("locations", [
+            "zipcode" =>  $zipcode,
+            "address" => $address,
+            "user_id" => $user_id,
+        ]);
+        $response->assertRedirect(route('users.show', ['name' => $user->name]));
+    }
 }
