ecba85508f06c5ab1fa454f292477968b1c1cb40
nozomi <kkk@docomo.ne.jp>
2021-03-24 16:30:08 +0900

    vue.js導入

--- backend/resources/views/locations/create.blade.php
+++ backend/resources/views/locations/create.blade.php
@@ -15,11 +15,15 @@
                 @csrf
                 <div class="md-form">
                   <label>天気を知りたい場所の郵便番号(ハイフンなし)を入力</label>
-                  <input type="text" name="zipcode" class="form-control" required value="{{ $location->zipcode ?? old('zipcode') }}" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11')";>
+                  <input type="text" name="zipcode" class="form-control" required value="{{ $location->zipcode ?? old('zipcode') }}" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11')">
                 </div>
                   <div class="md-form">
                   <label>位置情報（自動入力）</label>
                   <input type="text" name="addr11" class="form-control" readonly required value="{{ $location->address ?? old('address') }}">
+                </div>
+                <div class="md-form">
+                  <location-tags-input>
+                  </location-tags-input>
                 </div>
                 <button type="submit" class="btn blue-gradient btn-block">登録する</button>
               </form>
