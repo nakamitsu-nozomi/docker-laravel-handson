96c499de7c856579f466b321263dedeb020e5fb9
nozomi <kkk@docomo.ne.jp>
2021-03-27 09:33:07 +0900

    tagページ作成

--- backend/app/Tag.php
+++ backend/app/Tag.php
@@ -3,6 +3,7 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Tag extends Model
 {
@@ -13,5 +14,9 @@ class Tag extends Model
     public function getHashtagAttribute(): string
     {
         return "#" . $this->name;
+    }
+    public function locations(): BelongsToMany
+    {
+        return $this->belongsToMany('App\Location')->withTimestamps();
     }
 }
---
+++ backend/resources/views/tags/show.blade.php
@@ -0,0 +1,18 @@
+@extends('app')
+
+@section('title', $tag->hashtag)
+
+@section('content')
+  <div class="container">
+    <div class="card mt-3">
+      <div class="card-body">
+        <h2 class="h4 card-title m-0">{{ $tag->hashtag }}</h2>
+        <div class="card-text text-right">
+
+          {{ $tag->locations->count()}}件
+        </div>
+      </div>
+    </div>
+
+  </div>
+@endsection
