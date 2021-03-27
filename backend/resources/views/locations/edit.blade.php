fa91b6348b6191712f53d3678575b087f2c72a1c
nozomi <kkk@docomo.ne.jp>
2021-03-27 10:05:01 +0900

    ビューをリファクタリング

--- backend/resources/views/locations/edit.blade.php
+++ backend/resources/views/locations/edit.blade.php
@@ -12,22 +12,7 @@
             <div class="card-text">
                 <form method="POST" action="{{ route('locations.update',["location"=> $location]) }}">
                 @method("PATCH")
-                @include("locations.form")
-                {{-- @csrf --}}
-                {{-- <div class="md-form">
-                  <label>天気を知りたい場所の郵便番号(ハイフンなし)を入力</label>
-                  <input type="text" name="zipcode" class="form-control" required value="{{ $location->zipcode ?? old('zipcode') }}" onKeyUp="AjaxZip3.zip2addr(this,'','addr11','addr11')">
-                </div>
-                  <div class="md-form">
-                  <label>位置情報（自動入力）</label>
-                  <input type="text" name="addr11" class="form-control" readonly required value="{{ $location->address ?? old('address') }}">
-                </div>
-                <div class="md-form">
-                  <location-tags-input
-                  :initial-tags='@json($tagNames ?? [])'
-                  >
-                  </location-tags-input> --}}
-        
+                @include("locations.form")    
                 <button type="submit" class="btn blue-gradient btn-block">変更する</button>
               </form>
             </div>
