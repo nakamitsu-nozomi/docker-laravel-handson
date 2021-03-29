5507992c773edaa7b79bf3333e4a83fc30a46df6
nozomi <kkk@docomo.ne.jp>
2021-03-29 09:54:00 +0900

    jqueryバグ修正

--- backend/resources/views/app.blade.php
+++ backend/resources/views/app.blade.php
@@ -22,9 +22,8 @@
     @yield('content')
   </div>
   <script src="{{ mix('js/app.js') }}"></script>  
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
   <!-- JQuery -->
-  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+  {{-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script> --}}
   <!-- Bootstrap tooltips -->
   <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.4/umd/popper.min.js"></script>
   <!-- Bootstrap core JavaScript -->
